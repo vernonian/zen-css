7e1b43f3839463dfdcc95d54a82036efe7241333
vernonian <121730895+vernonian@users.noreply.github.com>
2024-07-05 13:40:15 -0700

    Update gulpfile to combine all files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,17 @@ const combineAndMinify = () => {
   return gulp.src([
     './css/variables/colors.css',
     './css/variables/units.css',
+    './css/variables/elevation.css',
+    '/css/classes/colors.css',
+    '/css/classes/elevation.css',
+    '/css/classes/spaces.css',
+    '/css/classes/text.css',
+    '/css/classes/images.css',
+    '/css/classes/flex.css',
+    '/css/classes/grids.css',
+    '/css/classes/wrappers.css',
+    '/css/atoms/ubtton-link.css',
+    '/css/atoms/bento.css',
   ])
     .pipe(concat('zen.css'))
     .pipe(cleanCSS())
